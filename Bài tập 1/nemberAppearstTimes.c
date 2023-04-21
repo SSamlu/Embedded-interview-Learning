98ac62ddab8d8e016f1a86987bfbdcde857386a1
Trong Bui <trongbd@freemind.vn>
2023-04-21 11:27:46 +0700

    Homework1: nemberAppearstTimes

---
+++ Bài tập 1/nemberAppearstTimes.c
@@ -0,0 +1,29 @@
+#include <stdint.h>
+#include <stdio.h>
+// #include <stdlib.h>
+#define MAX_VALUE 10
+// int arr[10] = {1, 1, 2, 3, 4, 4}
+
+int count[MAX_VALUE] = {0};
+
+void MemberAppearstTimesCheck(const int* item, int size) {
+    for(int i; i < size; i++) {
+        (*(count + item[i]))++;
+    }
+}
+
+void PrintData() {
+    for (int i = 0; i < MAX_VALUE; i++) {
+        if(*(count + i) > 0) {
+            printf("so %d xuat hien %d lan trong mang arr \n", i, *(count + i));
+        }
+    }
+}
+int main() {
+    //int *arr = (int*) malloc(6 * sizeof(int));
+    int arr[] = {1, 1, 2, 3, 4, 4};
+    int arr_size = sizeof(arr)/sizeof(int);
+    MemberAppearstTimesCheck(arr, arr_size);
+    PrintData();
+    return 0;
+}
