7f4c9c2344149f58442cfc261c547fc43d66af73
Dien Nguyen Dinh <nguyendinhdien19980@gmail.com>
2023-06-23 21:50:07 +0700

    update

--- C/bai-tap-3/Date.c
+++ C/bai-tap-3/Date.c
@@ -8,19 +8,43 @@
 #include <stdint.h>
 #include <stdio.h>
 
-typedef enum {"Chu nhat", "Thu hai", "Thu ba", "Thu tu", "Thu nam", "Thu sau", "Thu bay"} dayOfWeekName;
-
 typedef struct {
   int year;
   int month; // ở đây có thể dùng uint8_t
   int day; // ở đây có thể dùng uint8_t
 } DateTime;
+
 /**
- *1. co ngay thang nam làm gốc (không thay đổi => const)
- *2. tháng 31 ngày, kiêm tra năm đó nhuận không
- *3. lấy này nhập - có ngày gốc rồi "%7" (chia lấy dư)
- *4. 
+ * Function: randomArrayGenerate
+ * Description: Create a array RaNdom with length random
+ * Input:
+ *      val - an Array value with datatype by user define
 */
+
+int dayOfWeek(DateTime date) {
+  int tmp = (14 - date.month) / 12;
+  int y = date.year - tmp;
+  int m = date.month + 12 * tmp - 2;
+  int day = (date.day + y + y/4 - y/100 + y/400 + (31*m)/12) % 7;
+  return day;
+}
+
+/**
+ * Function: randomArrayGenerate
+ * Description: Create a array RaNdom with length random
+ * Input:
+ *      val - an Array value with datatype by user define
+*/
+
+int isLeapYear(int year) {
+  if((year % 4 == 0 && year % 100 != 0)|| year % 400 == 0) {
+    //  printf("%hd la nam nhuan.\n", year);
+     return 1;
+  } else {
+    // printf("%hd khong phai nam nhuan.\n", year);
+    return 0;
+  }
+}
 
 /**
  * Function: DayOfMonth
@@ -55,57 +79,35 @@ int DayOfMonth(int month, int year) {
 }
 
 /**
- * Function: randomArrayGenerate
- * Description: Create a array RaNdom with length random
- * Input:
- *      val - an Array value with datatype by user define
+ * Function: getDate
+ * Description: Print date
+ * Input: 
+ *      today - a struct DateTime
+ * Output:
+ *      None
 */
-
-int dayOfWeek(DateTime date) {
-  int tmp = (14 - date.month) / 12;
-  int y = date.year - tmp;
-  int m = date.month + 12 * tmp - 2;
-  int day = (date.day + y + y/4 - y/100 + y/400 + (31*m)/12) % 7;
-  return day;
-}
-
-/**
- * Function: randomArrayGenerate
- * Description: Create a array RaNdom with length random
- * Input:
- *      val - an Array value with datatype by user define
-*/
-
-int isLeapYear(int year) {
-  if((year % 4 == 0 && year % 100 != 0)|| year % 400 == 0) {
-    //  printf("%hd la nam nhuan.\n", year);
-     return 1;
-  } else {
-    // printf("%hd khong phai nam nhuan.\n", year);
-    return 0;
+void getDate(DateTime *today) {
+  const DateTime rootToday = {1, 1, 1};
+  const char *dayOfWeekName[] = {"Chu nhat", "Thu hai", "Thu ba", "Thu tu", "Thu nam", "Thu sau", "Thu bay"};
+  int daySinceRoot = 0;
+  for (int year = rootToday.year; year < today->year; year++) {
+    daySinceRoot += 365 + isLeapYear(year);
   }
+  for (int month = 1; month < today->month; month++) {
+    daySinceRoot += DayOfMonth(month, today->year);
+  }
+  daySinceRoot += today->day - 1;
+  int dayOfWeekNum = (dayOfWeek(rootToday) + daySinceRoot) % 7;
+  // printf("%d",dayOfWeekNum);
+  
+  printf("Ngay %d/%d/%hd la %s\n", today->day, today->month, today->year, dayOfWeekName[dayOfWeekNum]);
 }
 
 int main() {
-  const DateTime rootToday = {1, 1, 1};
-  char *dayOfWeekName[] = {"Chu nhat", "Thu hai", "Thu ba", "Thu tu", "Thu nam", "Thu sau", "Thu bay"};
   DateTime today;
   printf("Nhap ngay, thang, nam: ");
   scanf("%d %d %hd", &today.day, &today.month, &today.year);
   printf("Ngay thang nam vua nhap: %d/%d/%hd\n", today.day, today.month, today.year);
-  int daySinceRoot = 0;
-  for (int year = rootToday.year; year < today.year; year++) {
-    /* code */
-    daySinceRoot += 365 + isLeapYear(year);
-  }
-  for (int month = 1; month < today.month; month++){
-    /* code */
-    daySinceRoot += DayOfMonth(month, today.year);
-  }
-  daySinceRoot += today.day - 1;
-  int dayOfWeekNum = (dayOfWeek(rootToday) + daySinceRoot) % 7;
-  printf("%d",dayOfWeekNum);
-  
-  printf("Ngay %d/%d/%hd la %s\n", today.day, today.month, today.year, dayOfWeekName[dayOfWeekNum]);
+  getDate(&today);
   return 0;
 }
