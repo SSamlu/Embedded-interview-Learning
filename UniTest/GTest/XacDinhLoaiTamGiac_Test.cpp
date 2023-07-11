f38f011c854495489f2a2616977984d47199a2ee
Dien Nguyen Dinh <nguyendinhdien19980@gmail.com>
2023-07-11 16:06:22 +0700

    Unitest

---
+++ UniTest/GTest/XacDinhLoaiTamGiac_Test.cpp
@@ -0,0 +1,88 @@
+﻿#include <algorithm>
+#include "pch.h"
+void test(int a, int* b, int& c) {
+    a = 10;
+    *b = 20;
+    c = 30;
+}
+
+enum LoaiTamGiac {
+    TamGiacKhongTonTai,
+    TamGiacDeu,
+    TamGiacCan,
+    TamGiacVuong,
+    TamGiacThuong
+};
+
+LoaiTamGiac XacDinhLoaiTamGiac(double a, double b, double c) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+        return TamGiacKhongTonTai;
+    }
+
+    if (a == b && b == c) {
+        return TamGiacDeu;
+    }
+
+    if (a == b || b == c || c == a) {
+        return TamGiacCan;
+    }
+
+    double maxSide = std::max(a, std::max(b, c));
+    double sumSquares = a * a + b * b + c * c;
+
+    if (2 * maxSide * maxSide == sumSquares) {
+        return TamGiacVuong;
+    }
+
+    return TamGiacThuong;
+};
+
+TEST(XacDinhLoaiTamGiacCaseName, TamGiacKhongTonTaiName_1) {
+    double a = 0; double b = 1; double c = 3;
+    LoaiTamGiac tamgiac = XacDinhLoaiTamGiac(a, b, c);
+    EXPECT_EQ(tamgiac, TamGiacKhongTonTai);
+}
+TEST(XacDinhLoaiTamGiacCaseName, TamGiacKhongTonTaiName_2) {
+    double a = 4; double b = -1; double c = 1;
+    LoaiTamGiac tamgiac = XacDinhLoaiTamGiac(a, b, c);
+    EXPECT_EQ(tamgiac, TamGiacKhongTonTai);
+}
+TEST(XacDinhLoaiTamGiacCaseName, TamGiacKhongTonTaiName_3) {
+    double a = 3; double b = 1; double c = -3;
+    LoaiTamGiac tamgiac = XacDinhLoaiTamGiac(a, b, c);
+    EXPECT_EQ(tamgiac, TamGiacKhongTonTai);
+}
+
+TEST(XacDinhLoaiTamGiacCaseName, TamGiacDeuName) {
+    double a = 3; double b = 3; double c = 3;
+    LoaiTamGiac tamgiac = XacDinhLoaiTamGiac(a, b, c);
+    EXPECT_EQ(tamgiac, TamGiacDeu);
+}
+
+TEST(XacDinhLoaiTamGiacCaseName, TamGiacCanName_1) {
+    double a = 1; double b = 1; double c = 2;
+    LoaiTamGiac tamgiac = XacDinhLoaiTamGiac(a, b, c);
+    EXPECT_EQ(tamgiac, TamGiacCan);
+}
+TEST(XacDinhLoaiTamGiacCaseName, TamGiacCanName_2) {
+    double a = 2; double b = 1; double c = 1;
+    LoaiTamGiac tamgiac = XacDinhLoaiTamGiac(a, b, c);
+    EXPECT_EQ(tamgiac, TamGiacCan);
+}
+TEST(XacDinhLoaiTamGiacCaseName, TamGiacCanName_3) {
+    double a = 1; double b = 2; double c = 1;
+    LoaiTamGiac tamgiac = XacDinhLoaiTamGiac(a, b, c);
+    EXPECT_EQ(tamgiac, TamGiacCan);
+}
+
+TEST(XacDinhLoaiTamGiacCaseName, TamGiacVuongName_1) {
+    double a = 3; double b = 4; double c = 5;
+    LoaiTamGiac tamgiac = XacDinhLoaiTamGiac(a, b, c);
+    EXPECT_EQ(tamgiac, TamGiacVuong);
+}
+
+TEST(XacDinhLoaiTamGiacCaseName, TamGiacThuongName) {
+    double a = 2; double b = 3; double c = 4;
+    LoaiTamGiac tamgiac = XacDinhLoaiTamGiac(a, b, c);
+    EXPECT_EQ(tamgiac, TamGiacThuong);
+}
---
+++ UniTest/GTest/bubbleTest.cpp
@@ -0,0 +1,43 @@
+﻿#include "pch.h"
+#include <algorithm>
+class ArraySorter {
+private:
+    int* arr;
+    int size;
+
+public:
+    ArraySorter(int array[], int arraySize) : arr(array), size(arraySize) {}
+
+    void BubbleSort() {
+        for (int i = 0; i < size - 1; ++i) {
+            for (int j = 0; j < size - i - 1; ++j) {
+                if (arr[j] > arr[j + 1]) {
+                    // Hoán đổi giá trị của hai phần tử
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    void PrintArray() {
+        for (int i = 0; i < size; ++i) {
+            std::cout << arr[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+};
+TEST(ArraySorterCaseName, BubbleSortName) {
+    int arr[] = { 10, 15, 20, 14, 9, 7 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+    ArraySorter sort(arr, size);
+    sort.BubbleSort();
+
+    EXPECT_EQ(arr[0], 7);
+    EXPECT_EQ(arr[1], 9);
+    EXPECT_EQ(arr[2], 10);
+    EXPECT_EQ(arr[3], 14);
+    EXPECT_EQ(arr[4], 15);
+    EXPECT_EQ(arr[5], 20);
+}
